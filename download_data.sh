6860f33525a904cbf1657779a92f58cecf345d77
caoscott <scottcao@outlook.com>
2019-05-11 20:35:30 -0500

    Use aria2c instead of wget.

--- download_data.sh
+++ download_data.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-wget http://images.cocodataset.org/zips/train2014.zip -q --show-progress &
-wget http://images.cocodataset.org/zips/val2014.zip -q --show-progress &
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip -q --show-progress &
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip -q --show-progress &
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip -q --show-progress &
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip -q --show-progress
+# wget http://images.cocodataset.org/zips/train2014.zip -q --show-progress &
+# wget http://images.cocodataset.org/zips/val2014.zip -q --show-progress &
+# wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip -q --show-progress &
+# wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip -q --show-progress &
+# wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip -q --show-progress &
+# wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip -q --show-progress
+aria2c -x3a http://images.cocodataset.org/zips/train2014.zip http://images.cocodataset.org/zips/val2014.zip https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
 
 unzip -q '*.zip'
